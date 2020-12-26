ba1887f83afbab4ea5b6d5724d05c061bae2dabd
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-12-26 11:14:16 +0530

    Add files via upload

---
+++ Assignment-02/assignment2.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+void main()
+{
+// Declaration
+int i;
+float j;
+char hw[12];
+
+// Assignment
+i=5;
+j=3.14;
+strcpy(hw,"Hello World");
+
+// Print The Values
+printf("%d is an Interger\n",i);
+printf("%f is an Float\n",j);
+printf("%s is a String\n",hw);
+}
