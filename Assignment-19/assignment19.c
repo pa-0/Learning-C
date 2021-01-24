ddd0a49903a352de8a3fea0fca4ed4cb5affffb4
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2021-01-24 20:09:33 +0530

    Update assignment19.c

--- Assignment-19/assignment19.c
+++ Assignment-19/assignment19.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (void)
+void main()
 {
-	//create char pointer
 	char *ptr;
+	int size;
+	printf("Enter size of array : ");
+	scanf("%d",&size);	
+	ptr=malloc(size*sizeof(char));
 
-	//ptr is now the memory address of the beginning of this 10 char element array.
-	//we used the sizeof() function to make sure that the size of memory allocated is 10 units on any host
-	ptr = malloc(10 * sizeof(char));
-
-	//if malloc fails, our ptr pointer will be pointing towards a NULL value, this checks for that
 	if (ptr == NULL)
 	{
 		printf("Memory could not be allocated.");
-		return 1;
+		exit(1);
 	}
+
 	else 
-	{
-		printf("Memory was successfully allocated.");
-		//this makes sure we don't keep memory allocated that we're not using.
-		free(ptr);
-		return 0;
-	}
+		{
+			printf("Memory was successfully allocated.");
+			free(ptr);
+		}
 }
