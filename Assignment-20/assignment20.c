f5b9089d8dbb2a6db9cf3737b04b0aea0a100284
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2021-01-24 20:19:34 +0530

    Update assignment20.c

--- Assignment-20/assignment20.c
+++ Assignment-20/assignment20.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//define structure
-struct struct_name
+struct Struct
 {
 	int integer;
 	char character;
-	float floatie;
-//give an instance of the structure the name 'example'
-} example;
+	float floatchar;
+};
 
-int main (void)
+void main (void)
 {
-	//use the . notation to refer to the members of the 'example' instance of the 'struct_name' struct
-	example.integer = 50;
-	example.character = 'W';
-	example.floatie = 3.14;
+	struct Struct var;
 
-	printf("This is the int: %d", example.integer);
-	printf(", this is the char: %c", example.character);
-	printf(", this is the float: %.2f\n", example.floatie);
-	
-	return 0;
-	
+	printf("Enter Integer Value : ");
+	scanf("%d",&var.integer);
+
+	printf("Enter Character Value : ");
+	scanf(" %c",&var.character);
+
+	printf("Enter Float Value : ");
+	scanf("%f",&var.floatchar);	
+
+	printf("This is the int: %d, this is the char: %c, this is the float: %f\n", var.integer, var.character,var.floatchar);
+
 }
