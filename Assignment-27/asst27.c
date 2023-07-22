6ebbd6c3f032b892a93ba0e69a65704a4a6b35ad
nichejambinn <nichejambinn@macsimalist.local>
2023-07-22 14:33:24 -0400

    Asst27

--- Assignment-27/asst27.c
+++ Assignment-27/asst27.c
@@ -9,9 +9,9 @@ ssize_t write(int fd, const void *buf, size_t count) {
     good_write = dlsym(RTLD_NEXT, "write");
 
     if (strcmp((char *)buf, "Hello, world!\n") == 0) {
-        write_result = write(fd, "Goodbye, cruel world!\n", 22);
+        write_result = good_write(fd, "Goodbye, cruel world!\n", 22);
     } else {
-        write_result = write(fd, buf, count);
+        write_result = good_write(fd, buf, count);
     }
 
     return write_result;
