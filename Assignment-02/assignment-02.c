ce181bd44fa480db0a66e92b297192709eab3561
whyexis <chongyingxiang@gmail.com>
2023-11-12 19:16:21 -0500

    Assignment-02 completed

---
+++ Assignment-02/assignment-02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int variable_integer = 5;
+	float variable_float = 3.14;
+	char variable_string[] = "Hello, World!";
+
+	printf("%i is an integer!\n", variable_integer);
+	printf("%f is a float!\n", variable_float);
+	printf("%s is a char!\n", variable_string);
+
+	return 0;
+}
+
