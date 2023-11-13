fa1224d996b5097ee9f6e0cb1432feedf027c284
whyexis <chongyingxiang@gmail.com>
2023-11-13 04:19:18 -0500

    Assignment-03 extra credit completed

--- Assignment-03/assignment-03.c
+++ Assignment-03/assignment-03.c
@@ -1,14 +1,25 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-	char first_name[50];
-	char last_name[50];
-
+	char *buffer;
+	
+	buffer = (char *) malloc(101);
 	printf("Enter your first name: ");
-	scanf("%s", &first_name);
+	scanf(" %100[^\n]", buffer);
+	char *first_name = (char *) malloc(strlen(buffer) + 1);
+	strcpy(first_name, buffer);
+
+	buffer = (char *) realloc(buffer, 101);
 	printf("Enter your last name: ");
-	scanf("%s", &last_name);
+	scanf(" %100[^\n]", buffer);
+	char *last_name = (char *) malloc(strlen(buffer) + 1);
+	strcpy(last_name, buffer);
+
 	printf("Hello %s %s!", first_name, last_name);
+
+	free(buffer);
 
 	return 0;
 }
