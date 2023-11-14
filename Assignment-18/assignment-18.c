8ac5c671de46cdfcd0b933fca7df5c503749d512
whyexis <chongyingxiang@gmail.com>
2023-11-13 21:14:11 -0500

    Assignment-18 code 1 with puts

--- Assignment-18/assignment-18.c
+++ Assignment-18/assignment-18.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+// no difference between puts and printf
 
 void hello(void)
 {
-        char array[] = {72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33, '\0'};
+        char array[] = {72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33};
 
-        printf("%s", array);
+        puts(array);
 }
 
 int main(void) 
