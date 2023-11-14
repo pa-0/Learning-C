ba8fe15a436bb95dcd152053cb992660d5aa3673
whyexis <chongyingxiang@gmail.com>
2023-11-13 21:19:57 -0500

    Assignment-18 code 2 with puts

--- Assignment-18/assignment-18.c
+++ Assignment-18/assignment-18.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-// no difference between puts and printf
+// no difference between printf and puts
+// puts is the simple choice and adds a new line in the end and printfwrites the output from a formatted string.
 
 void hello(void)
 {
-        char array[] = {72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33};
+        char array[] = {72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33, '\0'};
 
         puts(array);
 }
