ede5a218786720ac02286f837dd1e5a033e72be2
I-dream-in-cod3 <147734328+I-dream-in-cod3@users.noreply.github.com>
2024-02-20 21:28:12 -0500

    Update C-Fundamentals.c

--- C-Fundamentals.c
+++ C-Fundamentals.c
@@ -6,10 +6,27 @@
 char obama[5];
 
 
-//printing stuff
-printf("");
+//printing stuff 
+int MyInt = 2;
+printf("%d",MyInt);
 //%s is for strings
 
 //get input 
 
   scanf("%s", &first);
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    float piee = 3.1400;
+    char hello[]  = "Ello, guvna";
+    printf("%d is an int.\n%f is a float.\n%s is a charmander, I mean a char.",a,piee,hello);
+    return 0;
+}
+
+//%d is for int
+//%f is for float
+//%s is for string
