453f55cc8beed6a02b6b38ad764a1fed26ce4d74
I-dream-in-cod3 <147734328+I-dream-in-cod3@users.noreply.github.com>
2024-02-20 21:22:59 -0500

    Create C-Fundamentals.c

---
+++ C-Fundamentals.c
@@ -0,0 +1,15 @@
+// Make this markdown later 
+//c fundies
+
+  //initialize char array aka string
+// strings are arrays for chars >. saying it twice 
+char obama[5];
+
+
+//printing stuff
+printf("");
+//%s is for strings
+
+//get input 
+
+  scanf("%s", &first);
