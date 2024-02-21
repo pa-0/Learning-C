2e2f0842b28ef28de42f98522076e4f8332ddf12
I-dream-in-cod3 <147734328+I-dream-in-cod3@users.noreply.github.com>
2024-02-20 20:59:41 -0500

    Create my answer.c

---
+++ Assignment-02/my answer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    float piee = 3.1400;
+    char hello[]  = "Ello, guvna";
+    printf("%d is an int.\n%f is a float.\n%s is a charmander, I mean a char.",a,piee,hello);
+
+    return 0;
+}
+// Questions after this execrcise 
+
+/*
+what does int main mean
+
+C fundies
+
+main code is inside main function 
+
+return 0 in main function
+
+
+*/
