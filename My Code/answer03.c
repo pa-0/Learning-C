4fc0270cd2d2ae09032f0029689be98ade4539eb
I-dream-in-cod3 <147734328+I-dream-in-cod3@users.noreply.github.com>
2024-02-23 13:41:13 -0500

    Update and rename answer03.md to answer03.c

--- My Code/answer03.c
+++ My Code/answer03.c
@@ -1,3 +1,4 @@
+/*
 ## Assignment 3
 Prompt the user to input their first and last name and then print them a welcome message. Try storing the input as a char variable's value.
 
@@ -6,10 +7,20 @@ Easy Mode: Allocate an arbitrary amount of indices to your char array and pray t
 Extra Credit: Dynamically allocate the array size of your char variable based on the length of the user's input.
 
 ## Example Output
-```c
-printf(Enter your
-                                 
+*/
+
+
+
+
+printf("Enter your first name":);
+scanf("%s", &first);    
+printf("Enter your last name:");
+
+
 Enter your first name: Jimmy
+
+
+  
 Enter your last name: Smith
 Hello Jimmy Smith!
-```
+
