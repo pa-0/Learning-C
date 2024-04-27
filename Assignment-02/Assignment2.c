9bb43d50fb2bb372e88edb27ea5c2ee35f065928
DanteChoppaXXX <only1dantechoppa@gmail.com>
2024-04-27 02:09:18 +0000

    Assignment2

---
+++ Assignment-02/Assignment2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num1 = 7;
+    float num2 = 3.14;
+    char greet[] = "Hello, World!";
+
+    printf("%d is an integer!\n", num1);
+    printf("%f is a float!\n", num2);
+    printf("%s is a char!", greet);
+
+    return 0;
+}
