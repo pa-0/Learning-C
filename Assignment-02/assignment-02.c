5785bbf8ba805e775c39e1e4c9ccc96502f4b687
julia <juliabiersuriano@gmail.com>
2024-09-02 16:49:32 -0400

    assignment 02

---
+++ Assignment-02/assignment-02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int integer     = 90;
+    float floating  = 0.3;
+    char character  = 'a';
+
+    printf("\"%d\" is an integer.\n", integer);
+    printf("\"%.1f\" is a float.\n", floating);
+    printf("\"%c\" is a character.\n", character);
+
+    return 0;
+}
