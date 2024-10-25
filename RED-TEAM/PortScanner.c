ea5e34619ef8ee4ff81172a1a77000a13d7120c4
DanteChoppaXXX <only1dantechoppa@gmail.com>
2024-10-25 10:33:11 +0000

    PortScanner

--- RED-TEAM/PortScanner.c
+++ RED-TEAM/PortScanner.c
@@ -2,25 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Function to create a list of ports from the specified range.
+char portsList(const int start, const int end, char *portList)
+{
+
+    int index = 0;
+
+    for (int i = start; i < end + 1; i++)
+    {
+        portList[index] = i;
+        index++;
+    }
+}
+
 int main()
 {
 
     char targetHost[100];
-    int targetPort;
-    int timeout = 5;
+    int start, end;             // Port range.
+    int size = end - start + 1; // Size of port list.
+    char portList[size];
+    char *pPortList = portList; // Pointer to port list array.
+    int timeout = 5;            // Connection timeout.
 
     printf("PORT SCANNER\n");
     printf("=============\n");
 
+    // Ask the user for the target IP Address.
     printf("Enter the target IP Address:\n");
     printf("============================\n");
     scanf("%s", &targetHost);
 
+    // Ask the user for the target IP Address.
     printf("Enter the port number:\n");
     printf("============================\n");
     scanf("%d", &targetPort);
 
     printf("Target => %s:%d\n", targetHost, targetPort);
+    portsList(start, end, pPortList);
 
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\n", portList[i]);
+    }
     return 0;
 }
