95f238f21c56b82e16de2436b09fa400cfc59bdc
DanteChoppaXXX <only1dantechoppa@gmail.com>
2024-10-26 06:15:37 -0400

    PortScanner

--- RED-TEAM/PortScanner.c
+++ RED-TEAM/PortScanner.c
@@ -1,6 +1,18 @@
+/*
+SIMPLE PORT SCANNER PROGRAM BY DANTECHOPPAXXX.
+==============================================
+This program scans a specific range of ports on a target IP or hostname to determine which ports are open, closed, or filtered, then saves the results in a txt file.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <errno.h>
+
 
 // Function to create a list of ports from the specified range.
 char portsList(const int start, const int end, unsigned long *portList)
@@ -15,20 +27,67 @@ char portsList(const int start, const int end, unsigned long *portList)
     }
 }
 
-struct PortStatus
-{
-    unsigned long port;
-    char status[100];
-};
+// Fucntion to scan port on target host.
+int scanPort(const char *ip, unsigned long *port){
+    int sockfd;
+    struct sockaddr_in server_addr;
+
+    // Create socket.
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Socket creation failed");
+        return -1;
+    }
+    
+    // Set up server address structure.
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0)
+    {
+        perror("Invalid IP address!");
+        close(sockfd);
+        return -1;
+    }
+    
+    // Attempt to connect to the server.
+    int connection_result = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    // Interpret the conection result.
+    if (connection_result == 0)
+    {
+        printf("Port %ld is OPEN on %s\n", port, ip);
+        close(sockfd);
+        return 0;
+    }
+    else if (errno == ECONNREFUSED)
+    {
+        printf("Port %ld is CLOSED on %s\n", port, ip);
+    }
+    else if (errno == ETIMEDOUT)
+    {
+        printf("Port %ld is FILTERED (timed out) on %s\n", port, ip);
+    }
+    else{
+        printf("Error occurred: %s\n", strerror(errno));
+    }
+    
+    close(sockfd);
+    return -1;
+
+}
+
+
+void writeResult(FILE *file, unsigned long *port, const char *status, const char *host){
+    fprintf(file, "Port %ld is %s on %s\n", port, status, host);
+}
 
 int main()
 {
 
     char targetHost[100];
+    char *pTargetHost = targetHost; // Pointer to target IP.
     int start, end;  // Port range.
-    int timeout = 5; // Connection timeout.
-
-    struct PortStatus portStatus;
 
     printf("PORT SCANNER\n");
     printf("=============\n");
@@ -53,14 +112,37 @@ int main()
     unsigned long *pPortList = portList; // Pointer to port list array.
 
     portsList(start, end, pPortList);
-
+    
+    // Open file for writing.
+    FILE *file = fopen("ScanResults.txt", "w");
+    if (file == NULL)
+    {
+        perror("Failed to open file!");
+        return 1;
+    }
+    
     for (int i = 0; i < size; i++)
     {
-
         /* Perform port scan. */
-
-        printf("===================\n");
-        printf("%s:%ld\n", targetHost, pPortList[i]);
+        int scanResult = scanPort(pTargetHost, pPortList[i]);
+        if (scanResult == 0)
+        {
+            writeResult(file, pPortList[i], "OPEN", pTargetHost);
+        }
+        else if (scanResult == -1)
+        {
+            writeResult(file, pPortList[i], "CLOSED", pTargetHost);
+        }
+        else{
+            writeResult(file, pPortList[i], "FILTERED", pTargetHost);
+        }
+        
     }
+
+    fclose(file);
+    printf("========================================\n");
+    printf("Scan results written to ScanResults.txt\n");
+    printf("========================================\n");
+
     return 0;
 }
