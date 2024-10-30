e6e7fdf6d99f5255649717aab7b6640b2f7c2629
DanteChoppaXXX <only1dantechoppa@gmail.com>
2024-10-30 01:50:40 +0000

    ChatProgram

--- RED-TEAM/SOCKET/client.c
+++ RED-TEAM/SOCKET/client.c
@@ -6,11 +6,13 @@
 #include <sys/socket.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <pthread.h>
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 6302
 
-int main(){
+int main()
+{
 
     int client_socket;
     struct sockaddr_in server_addr;
@@ -23,14 +25,15 @@ int main(){
         perror("Failed to create socket!\n");
         return -1;
     }
-    
+
     // Setup the server address structure.
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    
+
     // Connect to the server.
-    if(connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         perror("Connection Failed!\n");
         return -1;
     }
@@ -47,7 +50,7 @@ int main(){
         fgets(client_message, sizeof(client_message), stdin);
 
         // Send the message to the server.
-        if (send(client_socket,client_message,strlen(client_message), 0) < 0)
+        if (send(client_socket, client_message, strlen(client_message), 0) < 0)
         {
             perror("Failed to send message to the client!");
             break;
@@ -55,15 +58,16 @@ int main(){
 
         memset(server_message, 0, sizeof(server_message));
 
-        if(recv(client_socket, server_message, sizeof(server_message), 0) < 0){
+        if (recv(client_socket, server_message, sizeof(server_message), 0) < 0)
+        {
             perror("Couldn't receive message from server!\n");
             break;
         }
 
         printf("Server: %s\n", server_message);
     }
-    
+
     close(client_socket);
-    
+
     return 0;
 }
--- RED-TEAM/SOCKET/server.c
+++ RED-TEAM/SOCKET/server.c
@@ -6,64 +6,20 @@
 #include <sys/socket.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <pthread.h>
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 6302
+#define MAX_CLIENTS 5
 
-int main()
+void *clientHandler(void *socket_desc)
 {
-
-    int sockfd, client_socket, c;
-    struct sockaddr_in server_addr, client_addr;
-    char *message;
+    int client_socket = *(int *)socket_desc;
     char client_message[2000], server_message[2000];
 
-    // Create a socket.
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        perror("Failed to create socket!\n");
-        return -1;
-    }
-
-    // Setup the server address structure.
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(SERVER_PORT);
-
-    // Bind the socket to the server IP and port.
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("Bind failed");
-        return 1;
-    }
-    printf("Server is ready and bound to port %d\n", SERVER_PORT);
-    printf("=======================================\n");
-
-    // Listen for incoming connections.
-    listen(sockfd, 3);
-
-    printf("Waiting for incoming connections...\n");
-    printf("====================================\n");
-
-    c = sizeof(struct sockaddr_in);
-
-    // Accept incoming client connections.
-    client_socket = accept(sockfd, (struct sockaddr *)&client_addr, (socklen_t *)&c);
-    if (client_socket < 0)
-    {
-        perror("Failed to accept client connection!\n");
-        return -1;
-    }
-
-    printf("Connection Accepted!\n");
-
+    // Handle client communication.
     while (1)
     {
-        // // Send a welcome message to the client.
-        // message = "Hello Neo, Welcome to The Matrix!\n";
-        // write(client_socket, message, strlen(message));
-
         memset(client_message, 0, sizeof(client_message));
 
         // Receive a message from the client.
@@ -86,10 +42,75 @@ int main()
             break;
         }
     }
-
-    // Close the sockets after handling the client
+    // Close client socket and free memory
     close(client_socket);
-    close(sockfd);
+    free(socket_desc);
+    pthread_exit(NULL);
+}
+
+int main()
+{
+
+    int server_socket, client_socket, *new_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+    // char *message;
+
+    // Create server socket.
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
+    {
+        perror("Failed to create socket!\n");
+        return -1;
+    }
+
+    // Setup the server address structure.
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    // Bind the socket to the server IP and port.
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Bind failed");
+        close(server_socket);
+        return 1;
+    }
+    printf("Server is ready and bound to port %d\n", SERVER_PORT);
+    printf("=======================================\n");
+
+    // Listen for incoming connections.
+    listen(server_socket, 3);
+
+    printf("Waiting for incoming connections...\n");
+    printf("====================================\n");
+
+    // Accept incoming client connections.
+    while ((client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_size)))
+    {
+        printf("Connection Accepted!\n");
+
+        // Allocate memory for new client socket.
+        new_socket = malloc(sizeof(int));
+        *new_socket = client_socket;
+
+        // Create new thread for the client.
+        pthread_t client_thread;
+        if (pthread_create(&client_thread, NULL, clientHandler, (void *)new_socket) < 0)
+        {
+            perror("Failed to create thread!\n");
+            free(new_socket);
+            return 1;
+        }
+    }
+    if (client_socket < 0)
+    {
+        perror("Failed to accept client connection!\n");
+        return -1;
+    }
+
+    // Close the socket after handling the client
+    close(server_socket);
 
     return 0;
 }
