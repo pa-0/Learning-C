e41f1fdc22f5a76feee6b0150661b178b4e4aa72
Peter Abbasi <poa0@icloud.com>
2024-11-05 16:20:06 -0600

    Add alt solutions to assignments 2 and 9 (#6)
    -

---
+++ Assignment-02/assignment2-alt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int integer = 5;
+  float floatvar = 3.14;
+  char string[] = "Hello, World!";
+
+ printf("%d is an intgeger!\n%.2f is a float!\n%s is a char array!\n",integer,floatvar,string);
+  return 0;
+}
